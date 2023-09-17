358d6849d3fd20f33d18a85282b265c503497e76
matucarbajal11 <matucarbajal11@gmail.com>
2023-09-17 14:17:36 -0300

    first commit

---
+++ campsula.py
@@ -0,0 +1,15 @@
+# Datos del personaje
+nombre = "Goku(kakaroto)"
+edad = 37
+profesion = "protectos de la tierra"
+lugar_de_origen = "planeta vegeta"
+hobby = "pelear"
+
+# Descripción del personaje
+
+#Eprimero lo hize de esta forma pero despues los hice de la otra forma para ahorrar lineas de codigo
+
+#descripcion = "El es " + nombre + ", un " + profesion + " de " + str(edad) + " años de edad, originario de " + lugar_de_origen + ",es uno de lo gerreros mas fuerte del universo" + hobby + ". Es una persona amable y siempre está dispuesto a ayudar a los demás."
+
+# Imprimir la descripción del personaje
+print   ("el es, " + nombre + "un" + profesion + " de " + str(edad) + " años de edad, originario de " + lugar_de_origen + ", es uno de los guerreros mas fuerte de el universo" + hobby + ".Es una persona amable y simepre esta dispuesta a ayudar a los demas " ) 
